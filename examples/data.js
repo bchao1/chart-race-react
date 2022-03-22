991fcf9b9b1b0565309ea7376cf99ca308c9c059
Manan Kinger <iamdrew259@gmail.com>
2022-03-22 12:27:20 +0530

    fix export usage

--- examples/data.js
+++ examples/data.js
@@ -1,1 +1,11 @@
-export default data = ['apple', 'banana', 'orange'].reduce((res, item) => ({...res, ...{[item]: Array(20).fill(0).map(_ => Math.floor(20 * Math.random()))}}), {});
+export default ["apple", "banana", "orange"].reduce(
+  (res, item) => ({
+    ...res,
+    ...{
+      [item]: Array(20)
+        .fill(0)
+        .map((_) => Math.floor(20 * Math.random())),
+    },
+  }),
+  {}
+);
